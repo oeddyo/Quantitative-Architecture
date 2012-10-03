ca57b9f37cae7332eb6db908209b2f934e698bf2
eddie.xie <oeddyo@gmail.com>
2012-10-02 23:25:11 -0400

    add temporal csv file

--- temporal_data.py
+++ temporal_data.py
@@ -6,19 +6,19 @@ from lib.mysql_connect import connect_to_mysql
 from datetime import timedelta
 import datetime
 import sys
+import csv
 
 def export_temporal_data(time_window, start_time, end_time):
     fetched_results = get_all_stats()
     ids = get_all_foursquare_ids()
-    #cur_time = datetime.datetime.now()
-    cur_time = start_time
     venue_dic = {}
     for r in ids.keys():
         venue_dic[r] = [] 
     
+    cur_time = start_time
     """round time to nearest hour/day/week"""
     if time_window == 'hour':
-        time_window = timedelta(hours=1)
+        time_window = timedelta(hours=2)
         cur_time = cur_time - timedelta(minutes=cur_time.minute, seconds=cur_time.second, microseconds = cur_time.microsecond) + timedelta(hours=1)
     elif time_window == 'day':
         time_window = timedelta(days=1)
@@ -29,6 +29,7 @@ def export_temporal_data(time_window, start_time, end_time):
      
     data_type = 'checkinsCount'
     time_list = []
+    time_list.append('Plaza/Time')
     while cur_time > start_time and cur_time <= end_time:
         print cur_time
         time_list.append(cur_time)
@@ -44,6 +45,13 @@ def export_temporal_data(time_window, start_time, end_time):
                 value = 0
             venue_dic[id].append( value )
         cur_time+= time_window
+    f = open('./temporal_2hour.csv', 'wt')
+    writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+    writer.writerow( time_list)
+    
+    for key in venue_dic.keys():
+        venue_dic[key].insert(0, ids[key])
+        writer.writerow( venue_dic[key] )
     print data_type
     print time_list
     for id in ids.keys():
@@ -52,7 +60,7 @@ def export_temporal_data(time_window, start_time, end_time):
 def main():
     #export_temporal_data('week')
     #export_temporal_data('day')
-    start_time = datetime.datetime(2012,9,30,10,00,00)
-    end_time = datetime.datetime(2012,10,01,23,00,00)
+    start_time = datetime.datetime(2012,9,30,20,00,00)
+    end_time = datetime.datetime(2012,10,02,23,00,00)
     export_temporal_data('hour', start_time, end_time)
 main()
