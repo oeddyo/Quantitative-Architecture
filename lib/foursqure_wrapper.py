b28c6922f6c5bc03bfd5c87b7cc17b80269ca939
Eddie Xie <eddie@ip-10-85-130-224.localdomain>
2012-11-10 21:59:26 -0500

    ok

--- lib/foursqure_wrapper.py
+++ lib/foursqure_wrapper.py
@@ -12,39 +12,29 @@ def default_client():
 
 def download_meta_data(venue_id, client = default_client()):
     """Download meta data from foursquare API"""
-    client = default_client()
-    print client.venues(venue_id)
-    pass
-    try:
-        venue = client.venues(venue_id)
-    except Exception as e:
-        return e
-    return [venue]
+    venue = client.venues(venue_id)
+    return venue
 
 def download_venue_photo(venue_id, client = default_client()):
     """Download photos for a venue using foursquare API"""
-    venue_photos = []
-    try:
-        for offset in range(0, 1000, 200):
-            venue_photo = self.client.venues.photos(VENUE_ID = venue_id, params = {'limit':200, 'group':'venue', 'offset':offset})
-            venue_photo.append(venue_photo)
-    except:
-        pass
-    return venue_photos
+    for offset in range(0, 1000, 200):
+        venue_photo = client.venues.photos(VENUE_ID = venue_id, params = {'limit':200, 'group':'venue', 'offset':offset})
+        yield venue_photo
 
 def download_venue_tips(venue_id, client = default_client()):
     venue_tips = []
-    try:
-        for offset in range(0,1000,100):
-            venue_tip = self.client.venues.tips(VENUE_ID = venue_id, params = {'limit':100, 'offset':offset} )
-            venue_tips.append(venue_tip)
-    except:
-        pass
-    return venue_tips
+    for offset in range(0,1000,100):
+        venue_tip = client.venues.tips(VENUE_ID = venue_id, params = {'limit':100, 'offset':offset} )
+        yield venue_tip
 
 if __name__=='__main__':
     print 'Testing:'
     print 'Test download meta data'
     print download_meta_data('4b903af5f964a520b77d33e3')
+
+    gen  = download_venue_photo('4b903af5f964a520b77d33e3')
+
+    while gen:
+        print gen.next()
     
 
