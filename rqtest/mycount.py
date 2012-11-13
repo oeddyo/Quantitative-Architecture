9f8895a02bdba045b79720695974f94b0df765b1
eddie.xie <oeddyo@gmail.com>
2012-11-13 10:57:55 -0500

    ok

--- rqtest/mycount.py
+++
@@ -1,9 +0,0 @@
-import requests
-def get_100():
-    print 'im in in '
-    return 100
-def count_words_at_url(url):
-    print 'im in'
-    resp = requests.get(url)
-    return len(resp.text.split())
-
